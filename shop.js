6bfc93cf84023d7062b39cc199e51dcb71a8511e
Miracle021 <plsshowme1@gmail.com>
2025-08-02 21:14:33 +0330

    Add files via upload

--- shop.js
+++ shop.js
@@ -4,3 +4,19 @@ function addToCart(product) {
   localStorage.setItem('cart', JSON.stringify(cart));
   alert(product + " added to cart!");
 }
+function checkout() {
+  let cart = JSON.parse(localStorage.getItem('cart')) || [];
+  alert("Order Confirmed! Items: " + cart.join(", "));
+  localStorage.clear();
+  document.getElementById('cart-list').innerHTML = "";
+}
+window.onload = function() {
+  if (document.getElementById('cart-list')) {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    document.getElementById('cart-list').innerHTML = cart.map(item => "<li>" + item + "</li>").join("");
+  }
+  if (document.getElementById('cart')) {
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+    document.getElementById('cart').innerText = cart.join(", ");
+  }
+}
